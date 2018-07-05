d0ee09a36c28aebbaaef816f600fbb6ee0cd6f78
lost273 <dnk273@gmail.com>
2018-07-05 17:45:04 +0300

    pagination

--- app/js/controllers/folderList.js
+++ app/js/controllers/folderList.js
@@ -1,14 +1,30 @@
 angular.module("linkHolder")
-    .controller("folderListCtrl", function ($scope, $filter) {
+    .constant("folderListActiveClass", "btn-primary")
+    .constant("linkListPageCount", 5)
+    .controller("folderListCtrl", function ($scope, $filter, 
+        folderListActiveClass, linkListPageCount) {
 
         var selectedFolder = null;
 
+        $scope.selectedPage = 1;
+        $scope.pageSize = linkListPageCount;
         $scope.selectFolder = function (newFolder) {
             selectedFolder = newFolder;
+            $scope.selectedPage = 1;
+        }
+
+        $scope.selectPage = function (newPage) {
+            $scope.selectedPage = newPage;
         }
 
         $scope.folderFilterFn = function (folder) {
             return selectedFolder == null ||
                 folder.name == selectedFolder;
+        }
+        $scope.getFolderClass = function(folder){
+            return selectedFolder == folder ? folderListActiveClass : "";
+        }
+        $scope.getPageClass = function (page) {
+            return $scope.selectedPage == page ? folderListActiveClass : "";
         }
     });
--- app/js/controllers/linkHolder.js
+++ app/js/controllers/linkHolder.js
@@ -3,12 +3,30 @@ angular.module("linkHolder")
 
     $scope.data = {
         folders:[
-            {"id": 7, "name": "tstfldr", "myLinks": [
+            {"id": 7, "name": "testfolder", "myLinks": [
                 {"id": 14, "description": "test1", "body": "test1"},
                 {"id": 15, "description": "test2", "body": "test2"}]  
             },
-            {"id": 4, "name": "tst", "myLinks": [
+            {"id": 4, "name": "folder0", "myLinks": [
                 {"id": 6, "description": "education test", "body": "www.tst.com"}]
+            },
+            {"id": 1, "name": "folder1", "myLinks": [
+                {"id": 6, "description": "education ", "body": "www.tst.com"}]
+            },
+            {"id": 1, "name": "folder2", "myLinks": [
+                {"id": 6, "description": "education ", "body": "www.tst.com"}]
+            },
+            {"id": 1, "name": "folder3", "myLinks": [
+                {"id": 6, "description": "education ", "body": "www.tst.com"}]
+            },
+            {"id": 1, "name": "folder4", "myLinks": [
+                {"id": 6, "description": "education ", "body": "www.tst.com"},
+                {"id": 6, "description": "education ", "body": "www.tst.com"},
+                {"id": 6, "description": "education ", "body": "www.tst.com"},
+                {"id": 6, "description": "education ", "body": "www.tst.com"},
+                {"id": 6, "description": "education ", "body": "www.tst.com"},
+                {"id": 6, "description": "education ", "body": "www.tst.com"},
+                {"id": 6, "description": "education ", "body": "www.tst.com"}]
             }
         ]
     };
