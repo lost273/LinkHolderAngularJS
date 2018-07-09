15ed97b94c9c5fd64c3702055f202483aa254245
lost273 <dnk273@gmail.com>
2018-07-09 17:57:15 +0300

    index

--- app/js/controllers/linkHolder.js
+++ app/js/controllers/linkHolder.js
@@ -3,14 +3,12 @@ angular.module("linkHolder")
 .controller("authCtrl", function ($scope, $http, tokenUrl) {
 
     $scope.authenticate = function (user, pass) {
-        $http.post(tokenUrl, {username: user, password: pass})
+        $http.post(tokenUrl, {grant_type: "password", username: user, password: pass})
             .then(function (response) {
                 $scope.data.folders = response;
                 console.log(response.access_token);
             },function (error) {
                 $scope.authenticationError = error;
-                //$scope.data.error = error;
-                console.log(error);
             });
     }
 });
